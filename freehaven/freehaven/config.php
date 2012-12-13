d0a982654d0a1531e266f781d20b3456733a22d3
tony baldwin <tony@tonybaldwin.org>
2012-12-12 19:10:27 -0500

    first commit

---
+++ freehaven/freehaven/config.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Theme settings
+ */
+
+
+
+function theme_content(&$a){
+	if(!local_user())
+		return;		
+	
+	$resize = get_pconfig(local_user(), 'freehaven', 'resize' );
+	$color = get_pconfig(local_user(), 'freehaven', 'color' );
+       $font_size = get_pconfig(local_user(), 'freehaven', 'font_size' );
+	$theme_width= get_pconfig(local_user(), 'freehaven', 'theme_width' );
+	
+	return freehaven_form($a,$color,$font_size,$resize,$theme_width);
+}
+
+function theme_post(&$a){
+	if(! local_user())
+		return;
+	
+	if (isset($_POST['freehaven-settings-submit'])){
+		set_pconfig(local_user(), 'freehaven', 'resize', $_POST['freehaven_resize']);	
+		set_pconfig(local_user(), 'freehaven', 'color', $_POST['freehaven_color']);
+		set_pconfig(local_user(), 'freehaven', 'font_size', $_POST['freehaven_font_size']);
+		set_pconfig(local_user(), 'freehaven', 'theme_width', $_POST['freehaven_theme_width']);
+	}
+}
+
+
+function theme_admin(&$a){
+	$resize = get_config('freehaven', 'resize' );
+	$color = get_config('freehaven', 'color' );
+	$font_size = get_config('freehaven', 'font_size' );
+	$theme_width= get_config('freehaven', 'theme_width' );
+	return freehaven_form($a,$color,$font_size,$resize,$theme_width);
+}
+
+function theme_admin_post(&$a){
+	if (isset($_POST['freehaven-settings-submit'])){
+		set_config('freehaven', 'resize', $_POST['freehaven_resize']);
+		set_config('freehaven', 'color', $_POST['freehaven_color']);
+		set_config('freehaven', 'font_size', $_POST['freehaven_font_size']);
+		set_config('freehaven', 'theme_width', $_POST['freehaven_theme_width']);
+
+	}
+}
+
+
+function freehaven_form(&$a, $color,$font_size,$resize,$theme_width){
+	$colors = array(
+		"freehaven"=>"freehaven", 
+	);
+	$font_sizes = array(
+		'12'=>'12',
+		"---"=>"---",
+		"16"=>"16",		
+		"14"=>"14",
+		'10'=>'10',
+		);
+	$resizes = array(
+		"0"=>"0 (no resizing)",
+		"600"=>"1 (600px)",
+		"300"=>"2 (300px)",
+		"250"=>"3 (250px)",
+		"150"=>"4 (150px)",
+	       );
+	$theme_widths =array (
+		"standard"=>"standard",
+		"narrow"=>"narrow",
+		"wide"=>"wide",
+		);
+
+	$t = file_get_contents( dirname(__file__). "/theme_settings.tpl" );
+	$o .= replace_macros($t, array(
+		'$submit' => t('Submit'),
+		'$baseurl' => $a->get_baseurl(),
+		'$title' => t("Theme settings"),
+		'$resize' => array('freehaven_resize',t ('Set resize level for images in posts and comments (width and height)'),$resize,'',$resizes),
+		'$font_size' => array('freehaven_font_size', t('Set font-size for posts and comments'), $font_size, '', $font_sizes),
+		'$theme_width' => array('freehaven_theme_width', t('Set theme width'), $theme_width, '', $theme_widths),
+		'$color' => array('freehaven_color', t('Color scheme'), $color, '', $colors),
+	));
+	return $o;
+}
---
+++ freehaven/freehaven/style.php
@@ -0,0 +1,140 @@
+<?php
+	$color=false;
+	$freehaven_font_size=false;
+	$freehaven_theme_width=false;
+
+	$site_color = get_config("freehaven","color");
+	$site_freehaven_font_size = get_config("freehaven", "font_size" );
+	$site_freehaven_theme_width = get_config("freehaven", "theme_width");
+	
+	if (local_user()) {
+		$color = get_pconfig(local_user(), "freehaven","color");
+		$freehaven_font_size = get_pconfig(local_user(), "freehaven", "font_size");
+		$freehaven_theme_width = get_pconfig(local_user(), "freehaven", "theme_width");
+	
+	}
+	
+	if ($color===false) $color=$site_color;
+	if ($color===false) $color="freehaven";
+	if ($freehaven_font_size===false) $freehaven_font_size=$site_freehaven_font_size;
+	if ($freehaven_theme_width===false) $freehaven_theme_width=$site_freehaven_theme_width;
+	if ($freehaven_theme_width===false) $freehaven_theme_width="standard";
+	
+		
+	if (file_exists("$THEMEPATH/$color/style.css")){
+		echo file_get_contents("$THEMEPATH/$color/style.css");
+	}
+
+
+
+	if($freehaven_font_size == "16"){
+		echo "
+			.wall-item-content-wrapper {
+  					font-size: 16px;
+  					}
+  					
+			.wall-item-content-wrapper.comment {
+  					font-size: 16px;
+  					}
+		";  
+       }
+       if($freehaven_font_size == "14"){
+		echo "
+			.wall-item-content-wrapper {
+  					font-size: 14px;
+  					}
+  					
+			.wall-item-content-wrapper.comment {
+  					font-size: 14px;
+  					}
+		";
+	}	
+	if($freehaven_font_size == "12"){
+		echo "
+			.wall-item-content-wrapper {
+  					font-size: 12px;
+  					}
+  					
+			.wall-item-content-wrapper.comment {
+  					font-size: 12px;
+  					}
+		";
+	}
+	if($freehaven_font_size == "10"){
+		echo "
+			.wall-item-content-wrapper {
+  					font-size: 10px;
+  					}
+  					
+			.wall-item-content-wrapper.comment {
+  					font-size: 10px;
+  					}
+		";
+	}
+	if ($freehaven_theme_width === "standard") {
+		echo "
+                     section {
+	                margin: 0px 10%;
+                       margin-right:10%;
+                       }
+
+                     aside {
+	                margin-left: 10%;
+                      }
+                     nav {
+	                margin-left: 10%;
+	                margin-right: 10%;
+
+                      }
+
+                     nav #site-location {
+	                right: 10%;
+
+                      }
+		";
+	}
+
+	if ($freehaven_theme_width === "narrow") {
+		echo "
+                     section {
+	                margin: 0px 15%;
+                       margin-right:15%;
+                       }
+
+                     aside {
+	                margin-left: 15%;
+                      }
+                     nav {
+	                margin-left: 15%;
+	                margin-right: 15%;
+
+                      }
+
+                     nav #site-location {
+	                right: 15%;
+
+                      }
+		";
+	}
+	if ($freehaven_theme_width === "wide") {
+		echo "
+                     section {
+	                margin: 0px 5%;
+                       margin-right:5%;
+                       }
+
+                     aside {
+	                margin-left: 5%;
+                      }
+                     nav {
+	                margin-left: 5%;
+	                margin-right: 5%;
+
+                      }
+
+                     nav #site-location {
+	                right: 5%;
+
+                      }
+		";
+	}
