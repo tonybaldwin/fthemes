f7b60dc51fe097fee4e4bb2842574b8c9139877d
Thomas Willingham <founder@kakste.com>
2012-12-23 15:32:36 +0000

    Implement smarty3, make config work, fix theme.php

--- freehaven/freehaven/config.php
+++ freehaven/freehaven/config.php
@@ -73,8 +73,14 @@ function freehaven_form(&$a, $color,$font_size,$resize,$theme_width){
 		"wide"=>"wide",
 		);
 
-	$t = file_get_contents( dirname(__file__). "/theme_settings.tpl" );
-	$o .= replace_macros($t, array(
+$t = get_markup_template("theme_settings.tpl" );
+	 	
+	 	$includes = array(
+	 	'$field_select' => 'field_select.tpl',
+	 	);
+	 	$includes = set_template_includes($a->theme['template_engine'], $includes);
+	 	
+	 	$o .= replace_macros($t, $includes + array(
 		'$submit' => t('Submit'),
 		'$baseurl' => $a->get_baseurl(),
 		'$title' => t("Theme settings"),
