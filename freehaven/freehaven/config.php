22979665d0e4ab51fc52a164f64ab9d7f739533a
Thomas Willingham <founder@kakste.com>
2012-12-29 15:09:37 +0000

    Fix config.php for changes in smarty.

--- freehaven/freehaven/config.php
+++ freehaven/freehaven/config.php
@@ -75,12 +75,8 @@ function freehaven_form(&$a, $color,$font_size,$resize,$theme_width){
 
 $t = get_markup_template("theme_settings.tpl" );
 	 	
-	 	$includes = array(
-	 	'$field_select' => 'field_select.tpl',
-	 	);
-	 	$includes = set_template_includes($a->theme['template_engine'], $includes);
 	 	
-	 	$o .= replace_macros($t, $includes + array(
+	 	$o .= replace_macros($t, array(
 		'$submit' => t('Submit'),
 		'$baseurl' => $a->get_baseurl(),
 		'$title' => t("Theme settings"),
